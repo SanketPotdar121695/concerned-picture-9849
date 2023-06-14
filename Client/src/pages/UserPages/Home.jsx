39ef043f12788149309530c747508d730b255b57
kharoteswati <kharoteswati31@gmail.com>
2023-06-14 23:51:18 +0530

    homepage

--- Client/src/pages/UserPages/Home.jsx
+++ Client/src/pages/UserPages/Home.jsx
@@ -1,7 +1,18 @@
+import { Img, Box, } from "@chakra-ui/react";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box w={"80%"} margin={"auto"} mt={"30px"}  >
+
+      <Img src="https://www.mygarden.com/sites/default/files/styles/msg_teaser_full_1200/public/lavender-oil-istock-826296566.jpg?h=01990d67&itok=5HWQvw3I"
+        borderRadius={"base"} />
+      <Box>
+        <h1>Making lavender Oil :Preserve the Scent og the South</h1>
+      </Box>
+
+    </Box>
+  )
 };
 
 export default Home;
--- Client/src/pages/UserPages/Signup.jsx
+++ Client/src/pages/UserPages/Signup.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+  Select,
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { Link as LinkNav, NavLink } from "react-router-dom";
 
-export default Signup;
+export default function SignupCard() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign up
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={3}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <FormControl id="age" isRequired>
+              <FormLabel>Age</FormLabel>
+              <Input type="number" />
+            </FormControl>
+            <FormControl id="City" isRequired>
+              <FormLabel>City</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl id="City" isRequired>
+              <FormLabel>Gender</FormLabel>
+              <Select>Gender
+                {/* <Option>Male</Option>
+                <Option>Female</Option>
+                <Option>Other</Option> */}
+
+              </Select>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'green.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'green.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user? <LinkNav color={'green.400'} to={"/login"}>Login</LinkNav>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
