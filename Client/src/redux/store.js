7064e3847c1ce57c0843083d473234ed562d6cd1
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-06-17 00:36:45 +0530

    Resolved on conflict in the store

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -2,16 +2,16 @@ import {
   compose,
   combineReducers,
   applyMiddleware,
-  legacy_createStore,
-} from "redux";
-import thunk from "redux-thunk";
+  legacy_createStore
+} from 'redux';
+import thunk from 'redux-thunk';
 
 import { authReducer } from './LoginReducer/authReducer';
-import { reducer as postReducer } from "./postReducer/reducer";
+import { reducer as postReducer } from './postReducer/reducer';
 
 const rootReducer = combineReducers({
-  authReducer
   postReducer,
+  authReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
