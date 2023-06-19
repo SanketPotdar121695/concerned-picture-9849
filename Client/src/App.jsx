cf03854d43a54de30e80953fa61d17bd54f44df8
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-06-19 09:49:56 +0530

    Used instances in the post reducer functions instead of axios

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,11 @@
-import Admin from "./ADMIN/Admin";
-import "./App.css";
-import { Footer } from "./components/Footer";
-import { Navbar } from "./components/Navbar";
-import UserRoutes from "./pages/AllRoutes/UserRoutes";
+import Admin from './ADMIN/Admin';
+import './App.css';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+import UserRoutes from './pages/AllRoutes/UserRoutes';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <UserRoutes />
       <Footer />
