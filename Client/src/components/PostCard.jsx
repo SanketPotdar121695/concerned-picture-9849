a59e6e93ad0f872c8a7d884ea8101c1c7d4c2e94
Sanket Potdar <sanket.potdar563@gmail.com>
2023-06-19 15:31:09 +0530

    Update PostCard.jsx

--- Client/src/components/PostCard.jsx
+++ Client/src/components/PostCard.jsx
@@ -1,8 +1,10 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Box, Button, Flex, Heading, Icon, Img, Text } from '@chakra-ui/react'
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
-export const PostCard = ({ title, image, category, rating }) => {
+export const PostCard = ({ title, image, category, rating, id }) => {
+    const navigate = useNavigate();
     return (
         <Flex shadow={"lg"} borderRadius="1rem" mt="20px" mb="20px" width={"100%"}>
             <Img w="280px" borderLeftRadius="1rem" src={image} />
@@ -13,7 +15,7 @@ export const PostCard = ({ title, image, category, rating }) => {
                 <Heading _hover={{ color: "green" }} >{title}</Heading>
                 <Flex alignItems={"center"}>
                     <Text size={"20px"}>Rating : {rating}</Text>
-                    <Button mt="10px" ml="auto" mb="20px" colorScheme='teal' w="130px" variant='outline' _hover={{
+                    <Button onClick={() => navigate(`/singlepost/:${id}`)} mt="10px" ml="auto" mb="20px" colorScheme='teal' w="130px" variant='outline' _hover={{
                         bg: 'green.400', color: "white"
                     }} > Learn More  <br /> <Icon as={ArrowForwardIcon} /></Button></Flex>
             </Flex>
